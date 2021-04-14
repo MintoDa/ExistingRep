d796aa817ac23c9e5bdb7b65cf4299bb01910d44
Daniel M <MintoDa@whs.school.nz>
2021-04-15 11:34:22 +1200

    Added in code to print "Hello World."

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * Prints out Hello World.
+ *
+ * @author Daniel M
+ * @version 15/4/2021
+ */
+public class HelloWorld
+{
+    public HelloWorld()
+    {
+        // initialise instance variables
+        System.out.println("Hello World");
+    }}
